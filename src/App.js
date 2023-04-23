a7bd6e5f452bc7d9439efe8af7ffc1bec107c358
Malik <miyanda97@gmail.com>
2023-04-23 22:26:06 +0100

    checked prject

--- src/App.js
+++ src/App.js
@@ -1,82 +1,10 @@
-// import './App.css';
-// import React, {useEffect, useState} from 'react';
-// import TodoForm from './Components/TodoForm';
-// import TodoList from './Components/TodoLists';
-// import { Typography } from '@material-ui/core/';
-
-
-// const LOCAL_STORAGE_KEY = "react-todo-list-todos";
-
-
-// function App() {
-//   const [todos, setTodos] = useState([]);
-
-//   //use effect with empty dependency array 
-//   //load todo because if no change in dependacy array effect is runs only once during initalisation.
-//   useEffect(() => {
-//     const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-//     if(storageTodos){
-//       setTodos(storageTodos)
-//     }
-    
-//   }, []);
-
-//   useEffect(()=> {
-//     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-//   }, [todos]);
-  
-//   function addTodo(todo) {
-//     setTodos([todo,...todos]);
-//   }
-
-//   function removeTodo(id){
-//     setTodos(todos.filter(todo => todo.id !== id))
-
-//   }
-
-//   function toggleComplete(id){
-//     setTodos(
-//       todos.map(todo => {
-//         if (todo.id === id){
-//           return{
-//             ...todo, 
-//             complete: !todo.completed
-//           };
-//         }
-//         return todo;
-//       })
-//     );
-//   }
-
-
-//   return (
-//     <div className="App">
-          
-//       <Typography style={{ padding: 16}} variant="h1">
-//         React Todo
-//       </Typography>
-
-//       <TodoForm addTodo={addTodo} />
-//       <TodoList 
-//         todos={todos} 
-//         toggleComplete={toggleComplete}
-//         removeTodo={removeTodo}
-//         />
-
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import Typography from "@material-ui/core/Typography";
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import TodoForm from "./Components/TodoForm";
 import TodoList from "./Components/TodoList";
 
+//git test
 const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 function App() {
